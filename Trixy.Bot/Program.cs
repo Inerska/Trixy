39afab360e019108013f02da8630102010aa6cf9
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2021-08-01 22:27:55 +0200

    feat: add bootstrapper

--- Trixy.Bot/Program.cs
+++ Trixy.Bot/Program.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using Remora.Discord.Gateway;
+using Remora.Discord.Gateway.Extensions;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Trixy.Bot
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CancellationTokenSource cancellationSource = new();
+
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                eventArgs.Cancel = true;
+                cancellationSource.Cancel();
+            };
+
+            string? botToken = Environment.GetEnvironmentVariable("TRIXYS_");
+
+            if (botToken is null)
+            {
+                throw new ArgumentNullException(nameof(botToken));
+            }
+
+            var services = new ServiceCollection()
+                .AddDiscordGateway(_ => botToken)
+                .BuildServiceProvider();
+
+            var gatewayClient = services.GetRequiredService<DiscordGatewayClient>();
+
+            var runResult = await gatewayClient.RunAsync(cancellationSource.Token);
         }
     }
 }
