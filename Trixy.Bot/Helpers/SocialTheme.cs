3031ec6def50acf246b0aa2bb9e7dc5a2acab6e8
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2021-08-05 13:41:42 +0200

    style(social-theme): stringify to expression method

--- Trixy.Bot/Helpers/SocialTheme.cs
+++ Trixy.Bot/Helpers/SocialTheme.cs
@@ -1,5 +1,6 @@
 using OneOf;
 using System;
+using System.ComponentModel;
 
 namespace Trixy.Bot.Helpers
 {
@@ -42,32 +43,30 @@ namespace Trixy.Bot.Helpers
             DANCE
         }
 
-        public static string Stringify(OneOf<SafeForWork, NotSafeForWork> category)
+        public static string Stringify(OneOf<SafeForWork, NotSafeForWork> category) => category.Value switch
         {
-            return category.Value switch
-            {
-                SafeForWork.BULLY => "bullies",
-                SafeForWork.CUDDLE => "cuddles",
-                SafeForWork.CRY => "cries",
-                SafeForWork.HUG => "hugs",
-                SafeForWork.KISS => "kisses",
-                SafeForWork.LICK => "licks",
-                SafeForWork.PAT => "pats",
-                SafeForWork.SMUG => "smugs",
-                SafeForWork.BLUSH => "blushes",
-                SafeForWork.SMILE => "smiles",
-                SafeForWork.WAVE => "waves",
-                SafeForWork.HIGHFIVE => "highfives",
-                SafeForWork.NOM => "noms",
-                SafeForWork.BITE => "bites",
-                SafeForWork.SLAP => "slaps",
-                SafeForWork.KILL => "kills",
-                SafeForWork.KICK => "kicks",
-                SafeForWork.WINK => "winks",
-                SafeForWork.POKE => "pokes",
-                SafeForWork.DANCE => "dances",
-                _ => throw new ArgumentNullException(nameof(category));
-            };
-        }
+            SafeForWork.BULLY => "bullies",
+            SafeForWork.CUDDLE => "cuddles",
+            SafeForWork.CRY => "cries",
+            SafeForWork.HUG => "hugs",
+            SafeForWork.KISS => "kisses",
+            SafeForWork.LICK => "licks",
+            SafeForWork.PAT => "pats",
+            SafeForWork.SMUG => "smugs",
+            SafeForWork.BLUSH => "blushes",
+            SafeForWork.SMILE => "smiles",
+            SafeForWork.WAVE => "waves",
+            SafeForWork.HIGHFIVE => "highfives",
+            SafeForWork.NOM => "noms",
+            SafeForWork.BITE => "bites",
+            SafeForWork.SLAP => "slaps",
+            SafeForWork.KILL => "kills",
+            SafeForWork.KICK => "kicks",
+            SafeForWork.WINK => "winks",
+            SafeForWork.POKE => "pokes",
+            SafeForWork.DANCE => "dances",
+            _ => throw new InvalidEnumArgumentException(nameof(category), (int)category.Value, typeof(SafeForWork)),
+        };
+
     }
 }
