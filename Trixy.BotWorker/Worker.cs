676bf978eabad6aeb4c96bb857bed590288fa9b5
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2021-08-05 16:34:48 +0200

    feat(worker): check if slash commands are supported

--- Trixy.BotWorker/Worker.cs
+++ Trixy.BotWorker/Worker.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Remora.Discord.Commands.Services;
 using Remora.Discord.Gateway;
 
 namespace Trixy.BotWorker
@@ -14,23 +15,43 @@ namespace Trixy.BotWorker
     {
         public Worker(
             ILogger<Worker> logger,
-            DiscordGatewayClient discordGatewayClient)
+            DiscordGatewayClient discordGatewayClient,
+            SlashService slashService)
         {
             _logger = logger;
             _discordGatewayClient = discordGatewayClient;
+            _slashService = slashService;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            var slashSupported = _slashService.SupportsSlashCommands();
+            if (!slashSupported.IsSuccess)
+            {
+                _logger.LogCritical
+                    (
+                        "The registered commands don't support slash commands :(",
+                        slashSupported.Error.Message
+                    );
+
+                throw new Exception(slashSupported.Error?.Message, slashSupported.Error as Exception);
+            }
+
             var result = await _discordGatewayClient.RunAsync(stoppingToken);
             if (!result.IsSuccess)
-                throw new Exception(result.Error?.Message, result.Error as Exception);
+            {
+                _logger.LogCritical
+                    (
+                        "Oops... something went wrong during the connection of the gateway client.",
+                        result.Error.Message
+                    );
 
-            _logger.LogInformation("Trixy is ready to serve");
-            _logger.LogInformation(DateTime.Now.ToString(CultureInfo.CurrentCulture));
+                throw new Exception(result.Error?.Message, result.Error as Exception);
+            }
         }
 
         private readonly ILogger<Worker> _logger;
         private readonly DiscordGatewayClient _discordGatewayClient;
+        private readonly SlashService _slashService;
     }
 }
