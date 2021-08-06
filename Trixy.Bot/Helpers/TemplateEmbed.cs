c128b19464fd2932462767f68a66ed06818c33d3
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2021-08-06 10:57:51 +0200

    fix(about-embed): minor typos

--- Trixy.Bot/Helpers/TemplateEmbed.cs
+++ Trixy.Bot/Helpers/TemplateEmbed.cs
@@ -23,9 +23,9 @@ namespace Trixy.Bot.Helpers
         internal static async Task<Embed> GetAboutMeEmbed(string botAvatarUrl, Snowflake botId)
         {
             var builder = new StringBuilder();
-            builder.AppendLine($"Hello, I'm {botId.Mention()}, the bot will make your discord server in a new era of __modernity__");
+            builder.AppendLine($"Hello, I'm {botId.Mention()}, the bot that will make your discord server in a new era of __modernity__");
             builder.AppendLine("I dont have any prefix as my peers, you can access my commands with `/`, cool right ?\n");
-            builder.AppendLine("I'm open-source, you can contribute and raise an issue (even a typo) in my [GitHub repository](https://github.com/Inerska/Trixy)");
+            builder.AppendLine("I'm open-source, you can contribute to my development and raise an issue (even a typo) in my [gitHub repository](https://github.com/Inerska/Trixy)");
             builder.AppendLine("If you want to add me in your own server, you can just [click here](https://discord.com/api/oauth2/authorize?client_id=870605243891744859&permissions=8&scope=applications.commands%20bot)\n\n");
             builder.AppendLine("See you soon :)");
 
