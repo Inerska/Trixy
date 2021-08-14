0262ef719f2c07c80b5c639f18578475ee166137
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2021-08-14 14:19:40 +0200

    feat(responders): preparing bot servers register

---
+++ Trixy.Bot/Responders/GuildAddResponder.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Remora.Discord.API.Abstractions.Gateway.Events;
+using Remora.Discord.Gateway.Responders;
+using Remora.Results;
+
+namespace Trixy.Bot.Responders
+{
+    public class GuildAddResponder
+        : IResponder<IGuildCreate>
+    {
+        public Task<Result> RespondAsync(IGuildCreate gatewayEvent, CancellationToken ct = new CancellationToken())
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- Trixy.Bot/Responders/Setup.cs
+++ Trixy.Bot/Responders/Setup.cs
@@ -9,6 +9,7 @@ namespace Trixy.Bot.Responders
         internal static IServiceCollection AddTrixyResponders(this IServiceCollection services)
         {
             return services
+                .AddResponder<GuildAddResponder>(ResponderGroup.Early)
                 .AddResponder<ReadyResponder>(ResponderGroup.Early)
                 .AddResponder<MentionSelfResponder>();
         }
