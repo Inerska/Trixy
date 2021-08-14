bce7c1422beb5988e918f5e90a080c6f64d5daf4
Alexis Chân Gridel <perso.alexis.gridel@gmail.com>
2021-08-14 14:05:21 +0200

    feat(data): init data and efcore

---
+++ Trixy.DataAccess/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Trixy.DataAccess.Models
+{
+    public sealed record User(
+        [Required] int Id,
+        [Required] ulong Snowflake,
+        [Required] ulong Experience,
+        [Required] uint Level);
+}
+
+
+namespace System.Runtime.CompilerServices
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal class IsExternalInit
+    {
+    }
+}
---
+++ Trixy.DataAccess/TrixyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Trixy.DataAccess.Models;
+
+namespace Trixy.DataAccess
+{
+    public class TrixyDbContext
+        : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING_");
+            if (connectionString is null)
+                throw new ArgumentNullException(nameof(connectionString));
+
+            optionsBuilder.UseSqlite(connectionString);
+        }
+    }
+}
