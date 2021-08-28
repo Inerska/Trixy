14d7fcc6f0d7447b201e7d71f115dfc1e1d4f41d
Alexis C. GRIDEL <contact@agdl.dev>
2021-08-28 09:04:11 +0200

    feat(responder): attempt to register user feat concurency dal

---
+++ Trixy.Bot/Responders/FirstMessageRegistrationResponder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Remora.Discord.API.Gateway.Events;
+using Remora.Discord.Gateway.Responders;
+using Remora.Results;
+
+namespace Trixy.Bot.Responders
+{
+    public class FirstMessageRegistrationResponder
+        : IResponder<MessageCreate>
+    {
+        public Task<Result> RespondAsync(MessageCreate gatewayEvent, CancellationToken ct = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
