1429979468e9229e3cbb14f7598464d62155cbfa
Alexis C. GRIDEL <contact@agdl.dev>
2021-08-29 12:58:24 +0200

    feat(migrations): initial migration in debug same project

---
+++ Trixy.DataAccess/Migrations/20210829105607_InitialMigration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trixy.DataAccess.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Snowflake = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    Experience = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    Level = table.Column<uint>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
