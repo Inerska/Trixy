6137d1671cddf3def157977bfab88f41bdef9cb2
Alexis C. GRIDEL <contact@agdl.dev>
2021-08-29 22:01:07 +0200

    feat(dal): edit exp type

---
+++ Trixy.DataAccess/Migrations/20210829193646_ModifyExpTypes.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trixy.DataAccess.Migrations
+{
+    public partial class ModifyExpTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
